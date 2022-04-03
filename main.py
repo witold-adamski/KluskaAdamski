8bae0c25a2bfac8021080c97a2dc6cd3a8a46708
tak <tak@tak.pl>
2022-04-03 11:53:35 +0200

    tak

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-from flask import Flask
 from flask_restful import Resource, Api
 from service import FileService
+from flask import Flask, request
+from flasgger import Swagger, LazyString, LazyJSONEncoder
+from flasgger import swag_from
 
 app = Flask(__name__)
 api = Api(app)
+app.json_encoder = LazyJSONEncoder
+
+
+swagger_template = dict(
+info = {
+    'title': LazyString(lambda: 'My first Swagger UI document'),
+    'version': LazyString(lambda: '0.1'),
+    'description': LazyString(lambda: 'This document depicts a      sample Swagger UI document and implements Hello World functionality after executing GET.'),
+    },
+    host = LazyString(lambda: request.host)
+)
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'hello_world',
+            "route": '/hello_world.json',
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
+swagger = Swagger(app, template=swagger_template,
+                  config=swagger_config)
+
+@swag_from("hello_world.yml", methods=['GET'])
+@app.route("/")
+def hello_world():
+    return "Hello World!!!"
+
 
 
 class MoviesController(Resource):
