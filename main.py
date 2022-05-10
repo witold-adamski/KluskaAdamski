73dca57e04d86bcdbb0f65abc5ab18d0d9f6eb65
Marcin Kluska <marcin.kluska@edu.uekat.pl>
2022-05-10 23:28:00 +0200

    tak

--- main.py
+++ main.py
@@ -69,4 +69,4 @@ api = Api(app)
 
 if __name__ == '__main__':
     port = os.environ.get("PORT", 5000)
-    app.run(debug=False, host ="0,0,0,0", port=port)
+    app.run(debug=False, host ="0.0.0.0", port=port)
